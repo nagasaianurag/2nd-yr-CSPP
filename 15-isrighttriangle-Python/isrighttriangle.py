32cd04246a75ab2427d6dc6ebdf5db1fddf20466
NagaSaiAnurag <70886059+nagasaianurag@users.noreply.github.com>
2021-07-15 09:21:51 +0530

    half done

--- 15-isrighttriangle-Python/isrighttriangle.py
+++ 15-isrighttriangle-Python/isrighttriangle.py
@@ -7,4 +7,15 @@
 
 def isrighttriangle(x1, y1, x2, y2, x3, y3):
 	# your code goes here
-	pass
+	a = (((x2-x1)**2)+((y2-y1)**2))**0.5
+	b = (((x3-x2)**2)+((y3-y2)**2))**0.5
+	c = (((x3-x1)**2)+((y3-y1)**2))**0.5
+
+	if ((a**2) + (b**2) == (c**2)):
+		return True
+	elif ((a**2) + (c**2) == (b**2)):
+		return True
+	elif ((b**2) + (c**2) == (a**2)):
+		return True
+	else:
+		return False
