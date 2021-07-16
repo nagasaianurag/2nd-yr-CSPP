98f1bfe31005d1bb318461f7bdc7170783b7e906
NagaSaiAnurag <70886059+nagasaianurag@users.noreply.github.com>
2021-07-16 12:43:30 +0530

    done

--- 04-stringkeys-Python/stringkeys.py
+++ 04-stringkeys-Python/stringkeys.py
@@ -4,26 +4,24 @@ using the first two letters of the string."""
 
 class HashTable(object):
     def __init__(self):
-        self.table = [None]*10000
+        self.table = ['']*10000
 
     def store(self, string):
-        """Input a string that's stored in 
-        the table."""
+        """Input a string that's stored in the table."""
         # Hash Value = (ASCII Value of First Letter * 100) + ASCII Value of Second Letter 
-        # Your code goes here
-        pass
+        hashvalue = self.calculate_hash_value(string)
+        self.table[hashvalue] = string
+        return hashvalue
         
     def lookup(self, string):
-        """Return the hash value if the
-        string is already in the table.
+        """Return the hash value if the string is already in the table.
         Return -1 otherwise."""
-        # Your code goes here
-        pass
+        hashvalue = self.calculate_hash_value(string)
+        if(string in self.table[hashvalue]):
+            return hashvalue
+        return -1
 
     def calculate_hash_value(self, string):
-        """Helper function to calulate a
-        hash value from a string."""
-        # Your code goes here
-        pass
-
-
+        """Helper function to calulate a hash value from a string."""
+        hashvalue = ord(string[0])*100 + ord(string[1])
+        return hashvalue
