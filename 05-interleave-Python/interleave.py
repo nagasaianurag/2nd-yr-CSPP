0d65f7460b91441927438dda68684c9ef9b29702
NagaSaiAnurag <70886059+nagasaianurag@users.noreply.github.com>
2021-07-21 11:10:30 +0530

    done

--- 05-interleave-Python/interleave.py
+++ 05-interleave-Python/interleave.py
@@ -5,8 +5,20 @@
 # string onto the end of the new string. For example ('a#', 'cD!f2') would return 
 # the string "ac#D!f2". Assume that both s1 and s2 will always be strings.
 
-
-
 def fun_interleave(s1,s2):
-	return ""
+	result=""
+	a=len(s1)
+	b=len(s2)
+	if(a<b):
+		for i in range(a):
+			result = result+s1[i]+s2[i]
+		result+=s2[a:]
+	elif(a>b):
+		for i in range(b):
+			result = result+s1[i]+s2[i]
+		result+=s1[b:]
+	else:
+		for i in range(a):
+			result+=(s1[i]+s2[i])
+	return result
 	
