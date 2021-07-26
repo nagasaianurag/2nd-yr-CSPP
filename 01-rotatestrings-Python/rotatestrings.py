ffce131baf5615f676551965ef5af0eb4f4036d7
NagaSaiAnurag <70886059+nagasaianurag@users.noreply.github.com>
2021-07-26 09:51:11 +0530

    done

--- 01-rotatestrings-Python/rotatestrings.py
+++ 01-rotatestrings-Python/rotatestrings.py
@@ -4,8 +4,8 @@
 # assert(rotateString('abcd',  1) == 'bcda')
 # assert(rotateString('abcd', -1) == 'dabc')
 
-
-
 def fun_rotatestrings(s, n):
-	return s
-
+	# n = abs(n)
+	i = n%len(s)
+	left = (s[i:]+s[:i])
+	return left
