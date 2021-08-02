b2c21f63c5b0498d40dedd1b97d422e8eba36fd9
NagaSaiAnurag <70886059+nagasaianurag@users.noreply.github.com>
2021-08-02 09:46:31 +0530

    half done

--- 03-nth_smithnumber-Python/nth_smithnumber.py
+++ 03-nth_smithnumber-Python/nth_smithnumber.py
@@ -9,4 +9,18 @@
 
 
 def fun_nth_smithnumber(n):
+
     return 1
+
+def primeFactor(n):
+    for i in range(2,n + 1):
+        if n % i == 0:
+            count = 1
+            for j in range(2,(i//2 + 1)):
+                if(i % j == 0):
+                    count = 0
+                    break
+            if(count == 1):
+                return i
+    # return 
+print(primeFactor(27))
